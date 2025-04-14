9f7f7c54d0bf0b0d853fd836745b62d5480e2326
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-04-14 14:33:11 +0200

    opzet template

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
-console.log('Hier komt je server voor Sprint 10.')
+import express from 'express'
+import { Liquid } from 'liquidjs';
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
-console.log('Zet \'m op!')
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+app.set('views', './views')
+
+
+// MARK: Home
+app.get('/', async function (request, response) {
+    // connectie maken met server
+    // ... 
+    // data doorgeven aan de template
+
+    response.render('index.liquid',{
+
+    })
+})
+
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
